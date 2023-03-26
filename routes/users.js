19195ca3bc879e577ce35ddeb89dfc6cb74562d6
toto-O <phacusr3tr4@icloud.com>
2023-03-26 11:53:43 +0900

    add npm cors

--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,12 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  // res.set({ "Access-Control-Allow-Origin": "*" });
-  res.json({ message: "users" });
+  res.send({ message: "users" });
 });
 
 /* GET users listing. */
 router.post("/", function (req, res, next) {
-  res.header("Content-Type", "application/json; charset=utf-8");
   res.send("update message");
-  // res.render("users", data);
 });
 
 module.exports = router;
